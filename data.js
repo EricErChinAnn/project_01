9811acb07d5ab5ddba46ad6457f1ddfccf41295b
Eric Er <er_chin_ann@hotmail.com>
2022-10-17 23:15:39 +0800

    Pulling in Database and turning into Markers

--- data.js
+++ data.js
@@ -1,1 +1,37 @@
-let singaporeLL = [1.3521, 103.8198];
+const API_BASE_URL="https://api.foursquare.com/v3";
+const API_KEY="fsq36vWuvzc4ASQhuuv+TktgLmcCAI4WKFykx0ewzgChlkQ="
+const headers = {
+    Accept: 'application/json',
+    Authorization: API_KEY    
+}
+
+async function search(lat, lng, query) {
+    let ll = lat + "," + lng;
+    let response = await axios.get(API_BASE_URL + "/places/search",{
+        headers: {
+            ...headers
+        },
+        params: {
+            'll': ll,
+            'v': '20221017',  // YYYYMMDD format
+            'query': query,
+            'radius': 10000,
+            'limit': 50
+        }
+    })
+    return response.data;
+}
+
+const API_BASE_URL_PIC="https://api.foursquare.com/v3/places/";
+
+async function searchPic(fsq_id) {
+    let response = await axios.get(API_BASE_URL_PIC + fsq_id + "/photos",{
+        headers: {
+            ...headers
+        },
+        params: {
+            'fsq_id': fsq_id
+        }
+    })
+    return response.data;
+}
--- script.js
+++ script.js
@@ -1,10 +1,40 @@
-let map = L.map('map').setView(singaporeLL, 12);
+window.addEventListener('DOMContentLoaded', async () => {
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
-}).addTo(map);
+    let singapore = [1.3521, 103.8198];
+    let map = L.map('map').setView(singapore, 12);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoiZXh0cmFrdW4iLCJhIjoiY2swdnZtMWVvMTAxaDNtcDVmOHp2c2lxbSJ9.4WxdONppGpMXeHO6rq5xvg'
+    }).addTo(map);
+
+
+    let database = await search(1.3521, 103.8189, "gym");
+
+    let markerClusterLayer = L.markerClusterGroup();
+    markerClusterLayer.addTo(map);
+
+    let data = database.results;
+    for (let eachSearch of data) {
+        let lat = eachSearch.geocodes.main.latitude;
+        let lng = eachSearch.geocodes.main.longitude;
+        let searchMarker = L.marker([lat, lng]);
+        let picData = await searchPic(eachSearch.fsq_id)
+        searchMarker.bindPopup(`
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="${picData[0].prefix}original${picData[0].suffix}">
+            <div class="card-body">
+                <h5 class="card-title">${eachSearch.name}</h5>
+                <p class="card-text">${eachSearch.location.formatted_address}</p>
+            </div>
+      </div>
+      `)
+        searchMarker.addTo(markerClusterLayer);
+    }
+    
+
+})
