f6436290bf07b376c1c9df5d16db252cfa0c4646
Eric Er <er_chin_ann@hotmail.com>
2022-10-29 22:47:41 +0800

    Input validation and Landing Page with loading animation **Proud

--- data.js
+++ data.js
@@ -1,122 +1,122 @@
-const API_BASE_URL="https://api.foursquare.com/v3/places/";
-const API_KEY="fsq3CM63NagEqTAv1JgvG1gQJPq+g89o8J+VGpv9dzdZG3M=";
-const weatherAPI_KEY="a98a4d70a1d2cc9eca8357594caee0df";
+const API_BASE_URL = "https://api.foursquare.com/v3/places/";
+const API_KEY = "fsq3CM63NagEqTAv1JgvG1gQJPq+g89o8J+VGpv9dzdZG3M=";
+const weatherAPI_KEY = "a98a4d70a1d2cc9eca8357594caee0df";
 const headers = {
     Accept: 'application/json',
-    Authorization: API_KEY    
+    Authorization: API_KEY
 }
 
 let foodMarker = L.icon({
-    iconUrl:`img/marker/eats.png`,
+    iconUrl: `img/marker/eats.png`,
 
-    iconSize: [45,50],
-    iconAnchor: [22,40],
-    popupAnchor: [0,-30]
+    iconSize: [45, 50],
+    iconAnchor: [22, 40],
+    popupAnchor: [0, -30]
 });
 let allMarker = L.icon({
-    iconUrl:`img/marker/gym.png`,
+    iconUrl: `img/marker/gym.png`,
 
-    iconSize: [45,50],
-    iconAnchor: [22,40],
-    popupAnchor: [0,-30]
+    iconSize: [45, 50],
+    iconAnchor: [22, 40],
+    popupAnchor: [0, -30]
 });
 let boxingMarker = L.icon({
-    iconUrl:`img/marker/boxing.png`,
+    iconUrl: `img/marker/boxing.png`,
 
-    iconSize: [45,50],
-    iconAnchor: [22,40],
-    popupAnchor: [0,-30]
+    iconSize: [45, 50],
+    iconAnchor: [22, 40],
+    popupAnchor: [0, -30]
 });
 let bicycleMarker = L.icon({
-    iconUrl:`img/marker/bicycle.png`,
+    iconUrl: `img/marker/bicycle.png`,
 
-    iconSize: [45,50],
-    iconAnchor: [22,40],
-    popupAnchor: [0,-30]
+    iconSize: [45, 50],
+    iconAnchor: [22, 40],
+    popupAnchor: [0, -30]
 });
 let musicMarker = L.icon({
-    iconUrl:`img/marker/music.png`,
+    iconUrl: `img/marker/music.png`,
 
-    iconSize: [45,50],
-    iconAnchor: [22,40],
-    popupAnchor: [0,-30]
+    iconSize: [45, 50],
+    iconAnchor: [22, 40],
+    popupAnchor: [0, -30]
 });
 let pilateMarker = L.icon({
-    iconUrl:`img/marker/pilate.png`,
+    iconUrl: `img/marker/pilate.png`,
 
-    iconSize: [45,50],
-    iconAnchor: [22,40],
-    popupAnchor: [0,-30]
+    iconSize: [45, 50],
+    iconAnchor: [22, 40],
+    popupAnchor: [0, -30]
 });
 let climbMarker = L.icon({
-    iconUrl:`img/marker/rope.png`,
+    iconUrl: `img/marker/rope.png`,
 
-    iconSize: [45,50],
-    iconAnchor: [22,40],
-    popupAnchor: [0,-30]
+    iconSize: [45, 50],
+    iconAnchor: [22, 40],
+    popupAnchor: [0, -30]
 });
 let outdoorMarker = L.icon({
-    iconUrl:`img/marker/tree.png`,
+    iconUrl: `img/marker/tree.png`,
 
-    iconSize: [45,50],
-    iconAnchor: [22,40],
-    popupAnchor: [0,-30]
+    iconSize: [45, 50],
+    iconAnchor: [22, 40],
+    popupAnchor: [0, -30]
 });
 let yogaMarker = L.icon({
-    iconUrl:`img/marker/yoga.png`,
+    iconUrl: `img/marker/yoga.png`,
 
-    iconSize: [45,50],
-    iconAnchor: [22,40],
-    popupAnchor: [0,-30]
+    iconSize: [45, 50],
+    iconAnchor: [22, 40],
+    popupAnchor: [0, -30]
 });
 let weatherMarker = L.icon({
-    iconUrl:`img/marker/sunrise.png`,
+    iconUrl: `img/marker/sunrise.png`,
 
-    iconSize: [30,35],
-    iconAnchor: [15,28],
-    popupAnchor: [0,-30]
+    iconSize: [30, 35],
+    iconAnchor: [15, 28],
+    popupAnchor: [0, -30]
 });
 
-function selectedCategory(data){
-let categoriesList = {
-    "all":[18021,allMarker],
-    "boxing":[18022,boxingMarker],
-    "climbing":[18023,climbMarker],
-    "cycle":[18024,bicycleMarker],
-    "dance":[18025,musicMarker],
-    "outdoor":[18026,outdoorMarker],
-    "pilates":[18027,pilateMarker],
-    "yoga":[18028,yogaMarker]
-}
-return categoriesList[data];
+function selectedCategory(data) {
+    let categoriesList = {
+        "all": [18021, allMarker],
+        "boxing": [18022, boxingMarker],
+        "climbing": [18023, climbMarker],
+        "cycle": [18024, bicycleMarker],
+        "dance": [18025, musicMarker],
+        "outdoor": [18026, outdoorMarker],
+        "pilates": [18027, pilateMarker],
+        "yoga": [18028, yogaMarker]
+    }
+    return categoriesList[data];
 }
 
-function selectedWeather(data){
+function selectedWeather(data) {
     let obj = {
-        "clear sky":"☼",
-        "clear":"☼",
-        "few clouds":"🌥",
-        "clouds":"🌥",
-        "scattered clouds":"☁",
-        "broken clouds":"☁",
-        "shower rain":"🌦",
-        "rain":"🌧",
-        "thunderstorm":"⛈",
-        "snow":"❄",
-        "mist":"🌫"
+        "clear sky": "☼",
+        "clear": "☼",
+        "few clouds": "🌥",
+        "clouds": "🌥",
+        "scattered clouds": "☁",
+        "broken clouds": "☁",
+        "shower rain": "🌦",
+        "rain": "🌧",
+        "thunderstorm": "⛈",
+        "snow": "❄",
+        "mist": "🌫"
     }
     return obj[data];
 }
 
-async function search(lat, lng, categories, query="",radius="10000",limit="50") {
+async function search(lat, lng, categories, query = "", radius = "10000", limit = "50") {
     let ll = lat + "," + lng;
-    let response = await axios.get(API_BASE_URL + "search",{
+    let response = await axios.get(API_BASE_URL + "search", {
         headers: headers,
         params: {
             'll': ll,
             'v': '20221017',  // YYYYMMDD format
             'query': query,
-            'categories':categories,
+            'categories': categories,
             'radius': radius,
             'limit': limit
         }
@@ -124,23 +124,84 @@ async function search(lat, lng, categories, query="",radius="10000",limit="50") 
     return response.data;
 }
 
-async function searchPic(fsq_id){
-    let response = await axios.get(API_BASE_URL+fsq_id+"/photos",{
-        "headers":headers,
+async function searchPic(fsq_id) {
+    let response = await axios.get(API_BASE_URL + fsq_id + "/photos", {
+        "headers": headers,
         params: {
             'fsq_id': fsq_id
-                    }
+        }
     });
     return response.data;
 }
 
-async function weatherForecast(lat,lng,weatherAPI_KEY){
+async function weatherForecast(lat, lng, weatherAPI_KEY) {
     let database = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${weatherAPI_KEY}`);
     return database.data;
 }
 
-function kToCel(num){
+function kToCel(num) {
     num = Number(num);
     let returnNum = (num - 273.15).toFixed(2);
     return returnNum;
+}
+
+function initMap() {
+    let singapore = [1.3521, 103.8198];
+
+    let transitView = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        maxZoom: 20,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+    });
+
+    let simpleView = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+        maxZoom: 20,
+        attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    });
+
+    let roadView = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoiZXh0cmFrdW4iLCJhIjoiY2swdnZtMWVvMTAxaDNtcDVmOHp2c2lxbSJ9.4WxdONppGpMXeHO6rq5xvg',
+    })
+
+    let map = L.map('map', {
+        layers: [roadView, transitView, simpleView],
+        position: 'bottomright'
+    }).setView(singapore, 12);
+
+    var baseMaps = {
+        "Road": roadView,
+        "Detailed": transitView,
+        "Simple": simpleView
+    };
+
+    let layerControl = L.control.layers(baseMaps, {}, { position: 'bottomright' }).addTo(map);
+
+    return map;
+}
+
+function placeholderPic() {
+    let placeholderPics = ["img/wip1.jpg", "img/wip2.jpg", "img/wip3.jpg"];
+    let randomNum = Math.floor(Math.random() * placeholderPics.length);
+    return placeholderPics[randomNum];
+}
+
+async function quoteGetter() {
+    let quotesDatabase = (await axios.get(`quotes.json`)).data;
+    let randomGen = Math.floor(Math.random() * quotesDatabase.length);
+    let slogan = document.createElement("div");
+    let holder = document.querySelector(`#offcanvasTop`);
+    let loading = document.querySelector(`.center`);
+    holder.removeChild(loading);
+    slogan.innerHTML = `
+        <div class="container d-flex justify-content-center mt-0 pt-0 animate__animated animate__fadeIn">
+            <h5 id="slogan">${quotesDatabase[randomGen].text}</h5>
+        </div>
+        <div class="d-flex justify-content-end mt-0 pt-0 me-5 pe-5 animate__animated animate__fadeIn">
+            <h5 class="justify-content-end" id="sloganBy">- ${quotesDatabase[randomGen].author}</h5>
+        </div>`;
+    holder.appendChild(slogan);
 }
--- script.js
+++ script.js
@@ -1,26 +1,6 @@
-function initMap() {
-    let singapore = [1.3521, 103.8198];
-    let map = L.map('map').setView(singapore, 12);
-
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: 'pk.eyJ1IjoiZXh0cmFrdW4iLCJhIjoiY2swdnZtMWVvMTAxaDNtcDVmOHp2c2lxbSJ9.4WxdONppGpMXeHO6rq5xvg'
-    }).addTo(map);
-
-    return map;
-}
-
-function placeholderPic() {
-    let placeholderPics = ["img/wip1.jpg", "img/wip2.jpg", "img/wip3.jpg"];
-    let randomNum = Math.floor(Math.random() * placeholderPics.length);
-    return placeholderPics[randomNum];
-}
-
 window.addEventListener('DOMContentLoaded', async () => {
+    quoteGetter();
+
     let map = initMap();
     let markerClusterLayer = L.markerClusterGroup();
     markerClusterLayer.addTo(map);
@@ -57,56 +37,57 @@ window.addEventListener('DOMContentLoaded', async () => {
         let data = database.results;
         let resultDisplay = document.querySelector(`#resultDisplay`);
         resultDisplay.innerHTML = "";
-
-        for (let eachSearch of data) {
-            let lat = eachSearch.geocodes.main.latitude;
-            let lng = eachSearch.geocodes.main.longitude;
-            let searchMarker = L.marker([lat, lng], { icon: (selectedCateID[1]) }).addTo(markerClusterLayer);
-            searchMarker.addEventListener(`click`, () => {
-                map.flyTo([lat, lng], 18);
-            })
-            searchMarker.bindPopup(() => {
-                let ele = document.createElement('div');
-                ele.classList.add("card");
-                ele.style.width = "18rem";
-                ele.innerHTML += `
+        if (!(data[0] == undefined)) {
+            for (let eachSearch of data) {
+                let lat = eachSearch.geocodes.main.latitude;
+                let lng = eachSearch.geocodes.main.longitude;
+                let searchMarker = L.marker([lat, lng], { icon: (selectedCateID[1]) }).addTo(markerClusterLayer);
+                searchMarker.addEventListener(`click`, () => {
+                    map.flyTo([lat, lng], 18);
+                })
+                searchMarker.bindPopup(() => {
+                    let ele = document.createElement('div');
+                    ele.classList.add("card");
+                    ele.style.width = "18rem";
+                    ele.innerHTML += `
                 <div class="card-body">
                     <h5 class="card-title">${eachSearch.name}</h5>
                     <p class="card-text">${eachSearch.location.formatted_address}</p>
                 </div>`;
-                async function getPic() {
-                    let picData = await searchPic(eachSearch.fsq_id);
-                    let picURL = picData[0];
-                    if (picURL) {
-                        let picURLFull = picURL.prefix + "original" + picURL.suffix;
-                        ele.innerHTML += `<img class="card-img-top" src="${picURLFull}">`
-                    } else {
-                        ele.innerHTML += `<img class="card-img-top" src="${placeholderPic()}">`
+                    async function getPic() {
+                        let picData = await searchPic(eachSearch.fsq_id);
+                        let picURL = picData[0];
+                        if (picURL) {
+                            let picURLFull = picURL.prefix + "original" + picURL.suffix;
+                            ele.innerHTML += `<img class="card-img-top" src="${picURLFull}">`
+                        } else {
+                            ele.innerHTML += `<img class="card-img-top" src="${placeholderPic()}">`
+                        }
+
                     }
+                    getPic();
+                    return ele;
+                })
 
-                }
-                getPic();
-                return ele;
-            })
-
-            let clickOnHolder = document.createElement(`div`);
-            clickOnHolder.classList.add("card");
-            clickOnHolder.innerHTML =
-                `<div class="card-body d-flex flex-row justify-content-between p-2">
+                let clickOnHolder = document.createElement(`div`);
+                clickOnHolder.classList.add("card");
+                clickOnHolder.innerHTML =
+                    `<div class="card-body d-flex flex-row justify-content-between p-2">
                     <h5 class="card-title fs-6">${eachSearch.name}</h5>
                     <p class="btn btn-outline-info btn-sm card-text">Go</p>
                 </div>`;
-            clickOnHolder.addEventListener(`click`, () => {
-                map.flyTo([lat, lng], 18);
-                let resultDisplay = document.querySelector(`#resultDisplay`);
-                resultDisplay.style.display = "none";
-                setTimeout(() => {
-                    searchMarker.openPopup();
-                }, 3500)
-            })
+                clickOnHolder.addEventListener(`click`, () => {
+                    map.flyTo([lat, lng], 18);
+                    let resultDisplay = document.querySelector(`#resultDisplay`);
+                    resultDisplay.style.display = "none";
+                    setTimeout(() => {
+                        searchMarker.openPopup();
+                    }, 3500)
+                })
 
-            resultDisplay.appendChild(clickOnHolder);
-        }
+                resultDisplay.appendChild(clickOnHolder);
+            }
+        } else { alert(`There is no Gym or Studio in a 10 KM radius. Please center your view to another location`) }
     })
 
     let foodLayer = L.layerGroup();
@@ -118,52 +99,55 @@ window.addEventListener('DOMContentLoaded', async () => {
             let boundCenter = bound.getCenter();
             let boundCenterLat = boundCenter.lat;
             let boundCenterLng = boundCenter.lng;
-            L.circle([boundCenterLat, boundCenterLng], {
-                color: 'black',
-                fillColor: "black",
-                fillOpacity: 1,
-                radius: 1
-            }).addTo(foodLayer)
-            L.circle([boundCenterLat, boundCenterLng], {
-                color: 'black',
-                fillColor: "red",
-                fillOpacity: 0.2,
-                radius: 500
-            }).addTo(foodLayer)
             let database = await search(boundCenterLat, boundCenterLng, 13000, "", "500", "10");
             let data = database.results;
+            if (!(data[0] == undefined)) {
+                L.circle([boundCenterLat, boundCenterLng], {
+                    color: 'black',
+                    fillColor: "black",
+                    fillOpacity: 1,
+                    radius: 1
+                }).addTo(foodLayer)
+                L.circle([boundCenterLat, boundCenterLng], {
+                    color: 'black',
+                    fillColor: "red",
+                    fillOpacity: 0.2,
+                    radius: 500
+                }).addTo(foodLayer)
+                for (let eachFood of data) {
+                    let lat = eachFood.geocodes.main.latitude;
+                    let lng = eachFood.geocodes.main.longitude;
+                    let searchMarker = L.marker([lat, lng], { icon: foodMarker }).addTo(foodLayer);
 
-            for (let eachFood of data) {
-                let lat = eachFood.geocodes.main.latitude;
-                let lng = eachFood.geocodes.main.longitude;
-                let searchMarker = L.marker([lat, lng], { icon: foodMarker }).addTo(foodLayer);
-
-                searchMarker.addEventListener(`click`, () => {
-                    map.flyTo([lat, lng], 18);
-                })
-                searchMarker.bindPopup(() => {
-                    let ele = document.createElement('div');
-                    ele.classList.add("card");
-                    ele.style.width = "18rem";
-                    ele.innerHTML += `
+                    searchMarker.addEventListener(`click`, () => {
+                        map.flyTo([lat, lng], 18);
+                    })
+                    searchMarker.bindPopup(() => {
+                        let ele = document.createElement('div');
+                        ele.classList.add("card");
+                        ele.style.width = "18rem";
+                        ele.innerHTML += `
                     <div class="card-body">
                         <h5 class="card-title">${eachFood.name}</h5>
                         <p class="card-text">${eachFood.location.formatted_address}</p>
                     </div>`;
-                    async function getPic() {
-                        let picData = await searchPic(eachFood.fsq_id);
-                        let picURL = picData[0];
-                        if (picURL) {
-                            let picURLFull = picURL.prefix + "original" + picURL.suffix;
-                            ele.innerHTML += `<img class="card-img-top" src="${picURLFull}">`
-                        } else {
-                            ele.innerHTML += `<img class="card-img-top" src="img/foodWIP.png">`
-                        }
+                        async function getPic() {
+                            let picData = await searchPic(eachFood.fsq_id);
+                            let picURL = picData[0];
+                            if (picURL) {
+                                let picURLFull = picURL.prefix + "original" + picURL.suffix;
+                                ele.innerHTML += `<img class="card-img-top" src="${picURLFull}">`
+                            } else {
+                                ele.innerHTML += `<img class="card-img-top" src="img/foodWIP.png">`
+                            }
 
-                    }
-                    getPic();
-                    return ele;
-                })
+                        }
+                        getPic();
+                        return ele;
+                    })
+                }
+            } else {
+                alert(`There are no diners in a 500m radius, please change your view location`);
             }
 
         } else {
@@ -187,8 +171,8 @@ window.addEventListener('DOMContentLoaded', async () => {
             let mainWeather = (database.weather[0].main).toLowerCase();
             let weatherDisplay = document.querySelector(`#weatherDisplay`);
             let placeholderDiv = document.createElement(`div`);
-            placeholderDiv.classList.add("card","border-0","d-flex","flex-row","justify-content-evenly","align-items-center");
-            placeholderDiv.style.backgroundColor= "rgba(255,255,255,0)";
+            placeholderDiv.classList.add("card", "border-0", "d-flex", "flex-row", "justify-content-evenly", "align-items-center");
+            placeholderDiv.style.backgroundColor = "rgba(255,255,255,0)";
             placeholderDiv.innerHTML = `
                 <h1 class="fs-1 m-0">${selectedWeather(mainWeather)}</h1>
                 <div class="d-flex flex-column">
@@ -199,8 +183,8 @@ window.addEventListener('DOMContentLoaded', async () => {
             weatherDisplay.appendChild(placeholderDiv);
         } else {
             weatherLayer.clearLayers();
-            weatherDisplay.innerHTML="";
-            
+            weatherDisplay.innerHTML = "";
+
         }
     })
 
